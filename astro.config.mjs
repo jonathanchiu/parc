470873712d601db8c53b72e51a0e6e43da7425b2
Jonathan Chiu <jonathanchiu7@gmail.com>
2024-03-17 02:38:45 -0400

    Add hacky workaround for HMR in WSL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,15 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  // This is a workaround for a bug in WSL that prevents hot module reloading from working on Windows
+  // See: https://github.com/withastro/astro/issues/6043#issuecomment-1409498718
+  // Not sure how this will interact with other OSes
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
 });
